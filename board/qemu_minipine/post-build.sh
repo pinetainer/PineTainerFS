5fc3d0d22e98a6fd202983fbb41142ca48350b22
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-07-12 15:00:26 +0200

    qemu_minipine: Mayor cumplimiento del FHS 3.0 y optimizaciones Se han eliminado de la compilación de busybox algunas opciones innecesarias, y también se han creado scripts y capas de personalización del sistema de ficheros raíz para reducir el número de usuarios preexistentes del sistema, cumplir mejor el Filesystem Hierarchy Standard 3.0, simplificar el proceso de arranque, actualizar algunos ficheros de datos estáticos a partir del paquete netutils de Debian y modificar la información reportada en /etc/os-release.

---
+++ board/qemu_minipine/post-build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Información de versión más apropiada que la predeterminada
+printf "NAME=\"Linux\"\nVERSION=\"5.1.6\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"$2\"\nVARIANT_ID=\"$2\"" > "$1/usr/lib/os-release"

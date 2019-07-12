2607c9c17aa2faa25ed1d8a0732c25be6dabd916
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-07-12 19:31:02 +0200

    minipine: Configuraciones iniciales de U-boot Todavía no han sido probadas. Estos cambios se han basado en la defconfig de pine64 disponible en Buildroot. Más información en:
    - Repositorio de genimage
    - http://linux-sunxi.org/Bootable_SD_card
    - Repositorio de U-boot
    
    También se ha desactivado la configuración de programar el inicio
    de getty en los terminales, ya que no se usa.
    
    Quedaría por configurar el kernel, y comprobar que estas configuraciones
    son correctas.

---
+++ board/minipine/boot.cmd
@@ -0,0 +1,6 @@
+setenv bootargs earlyprintk=vga root=/dev/mmcblk0p2 rootwait
+
+fatload mmc 0 $kernel_addr_r Image
+fatload mmc 0 $fdt_addr_r sun50i-h6-pine-h64.dtb
+
+booti $kernel_addr_r - $fdt_addr_r
--- board/minipine/post-build.sh
+++ board/minipine/post-build.sh
@@ -1,1 +1,6 @@
-../qemu_minipine/post-build.sh
+#!/bin/sh
+# Información de versión más apropiada que la predeterminada
+printf "NAME=\"Linux\"\nVERSION=\"5.1.6\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
+
+# Eliminar servicio no útil
+rm "$1/etc/init.d/S20urandom"

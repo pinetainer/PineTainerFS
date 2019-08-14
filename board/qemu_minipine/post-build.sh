caa7522e96c581afbd0d27a200f669c8c876d79b
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-14 22:08:11 +0200

    Eliminar config. para QEMU, y actualizar la de Minipine La configuración de QEMU es inútil, ahora que tenemos la Pine H64 y el adaptador UART-USB para comunicarnos con U-Boot. Se ha reconfigurado U-Boot para una mejor optimización para nuestro caso de uso. También pasaremos a usar el kernel de Sunxi, que contiene los últimos parches que mejoran la compatibilidad con dispositivos Allwiner como la Pine, y se integrarán en futuras versiones oficiales de Linux. Todavía queda configurar ese kernel.

--- board/qemu_minipine/post-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Información de versión más apropiada que la predeterminada
-printf "NAME=\"Linux\"\nVERSION=\"5.1.6\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"$2\"\nVARIANT_ID=\"$2\"" > "$1/usr/lib/os-release"
-
-# Eliminar servicio no útil, ignorando errores si ya fue borrado
-rm "$1/etc/init.d/S20urandom" || true

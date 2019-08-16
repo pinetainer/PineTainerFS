67b7fac6e5263f14d2f1322e843cf5e06db17c2a
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-16 23:28:06 +0200

    minipine: boot.cmd: Simplificar y arreglar script

--- board/minipine/boot.cmd
+++ board/minipine/boot.cmd
@@ -1,6 +1,6 @@
-setenv bootargs console=ttyS0,115200n8 console=tty0 root=/dev/mmcblk0p2 rootwait earlycon=uart,mmio32,0x05000000
+setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk0p2 rootwait earlycon=uart,mmio32,0x05000000
 
-load mmc {$mmc_bootdev}:1 40080000 Image
-load mmc {$mmc_bootdev}:1 4fa00000 sun50i-h6-pine-h64.dtb
+load mmc $mmc_bootdev:1 40080000 Image
+load mmc $mmc_bootdev:1 4fa00000 sun50i-h6-pine-h64.dtb
 
 booti 40080000 - 4fa00000

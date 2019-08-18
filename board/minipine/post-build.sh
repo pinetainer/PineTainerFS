2bb0c99e501475e3f54c30b313c4e0ae2ae6e182
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-18 23:00:19 +0200

    minipine: post-build.sh: Actualizar versión y eliminar firm. innecesario

--- board/minipine/post-build.sh
+++ board/minipine/post-build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Información de versión más apropiada que la predeterminada
-printf "NAME=\"Linux\"\nVERSION=\"5.1.6\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
+printf "NAME=\"Linux\"\nVERSION=\"5.3-rc4\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
 
 # Eliminar servicio no útil, ignorando errores si ya fue borrado
 rm "$1/etc/init.d/S20urandom" || true
+
+# Eliminar archivos de firmware innecesarios
+find "$1/firmware/rtlwifi" -type f -name '*.bin' \! -name 'rtl8723bs_nic.bin' -exec rm {} \;

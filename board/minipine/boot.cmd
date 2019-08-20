00bcaf23c33598ac657e1418eed5a4a686616ab4
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-20 22:07:07 +0200

    minipine: boot.cmd: Tests de reanudadocon WDT y configuración de WDT

--- board/minipine/boot.cmd
+++ board/minipine/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk0p2 rootwait earlycon=uart,mmio32,0x05000000
+setenv bootargs console=ttyS0,115200n8 earlycon=uart,mmio32,0x05000000 root=/dev/mmcblk0p2 rootwait sunxi_wdt.timeout=10 test_suspend=mem
 
 load mmc $mmc_bootdev:1 40080000 Image
 load mmc $mmc_bootdev:1 4fa00000 sun50i-h6-pine-h64.dtb

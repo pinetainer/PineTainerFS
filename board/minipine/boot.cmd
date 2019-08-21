ae307b06695eb81dd0a6ca7e25d29208501a7a0c
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-21 18:08:55 +0200

    minipine: Arreglos menores de presentación También se ha eliminado una opción de depuración del kernel.

--- board/minipine/boot.cmd
+++ board/minipine/boot.cmd
@@ -1,4 +1,5 @@
 setenv bootargs console=ttyS0,115200n8 earlycon=uart,mmio32,0x05000000 root=/dev/mmcblk0p2 rootwait sunxi_wdt.timeout=16
+
 load mmc $mmc_bootdev:1 40080000 Image
 load mmc $mmc_bootdev:1 4fa00000 sun50i-h6-pine-h64.dtb
 

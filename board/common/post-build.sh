667677d0f1f5b846ac037791bc6233001c34de8e
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-25 14:45:00 +0200

    Mover configuraciones a directorios comunes. Renombrar minipine a pinelive Este cambio permitirá reutilizar la lógica de compilado común a PineLive y el SO final que se ejecutará en la tarjeta, además de los parches al kernel. De esta forma, variantes del SO a ejecutar en la Pine solo tendrán que contener ficheros propios para configuraciones que las distingan, y compartirán un núcleo Linux.

---
+++ board/common/post-build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Información de versión más apropiada que la predeterminada
+printf "NAME=\"Linux\"\nVERSION=\"5.3-rc5\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
+
+# Eliminar servicio no útil, ignorando errores si ya fue borrado
+rm "$1/etc/init.d/S20urandom" || true
--- board/minipine/post-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Información de versión más apropiada que la predeterminada
-printf "NAME=\"Linux\"\nVERSION=\"5.3-rc4\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
-
-# Eliminar servicio no útil, ignorando errores si ya fue borrado
-rm "$1/etc/init.d/S20urandom" || true

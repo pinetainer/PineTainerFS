a178eccd2a11c27fa9438a8f953984706867a43c
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-31 17:39:32 +0200

    common: post-build.sh: Eliminar contenido de /dev e iniciar random antes
    Ya que se monta devtmpfs sobre /dev, sus contenidos se ocultan y son
    irrelevantes. Por otro lado, resulta más provechoso iniciar el servicio
    de establecimiento de semillas aleatorias cuanto antes, para que el
    resto del sistema utilice la aleatoriedad generada.

--- board/common/post-build.sh
+++ board/common/post-build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # Información de versión más apropiada que la predeterminada
-printf "NAME=\"Linux\"\nVERSION=\"5.3-rc5\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
+printf "NAME=\"Linux\"\nVERSION=\"5.3-rc6\"\nID=linux\nID_LIKE=\"buildroot\"\nHOME_URL=\"https://github.com/pinetainer\"\nVARIANT=\"minipine\"\nVARIANT_ID=\"minipine\"" > "$1/usr/lib/os-release"
 
 # Borrar ficheros inútiles solo usados por bash
 rm -rf "$1/etc/bash_completion.d" || true
 
+# Hacer que urandom sea de los primeros servicios en ser iniciado,
+# para tener acceso a mayor aleatoriedad antes
+mv "$1/etc/init.d/S20urandom" "$1/etc/init.d/S01urandom.sh" || true
+
 # Borrar ficheros que cuelgan de directorios temporales
 rm -rf "$1/tmp/*" || true
 rm -rf "$1/run/*" || true
+find "$1/dev" -mindepth 1 -delete || true
 
 # Borrar rmt y el directorio que lo contiene, si queda vacío
 rm -f "$1/libexec/rmt" || true

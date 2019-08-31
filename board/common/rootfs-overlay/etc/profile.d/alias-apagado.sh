dd8ab02e056d32e4f3b5a4d4a5f2129d600d2057
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2019-08-31 23:27:55 +0200

    Hacer el reinicio por kexec utilizable realmente

--- board/common/rootfs-overlay/etc/profile.d/alias-apagado.sh
+++ board/common/rootfs-overlay/etc/profile.d/alias-apagado.sh
@@ -2,8 +2,10 @@
 
 # Estos alias aceleran ligeramente la operación,
 # y evitan que se mezcle el prompt del terminal
-# con el texto emitido por init
+# con el texto emitido por init. También implementan
+# coldreboot.
 alias reboot='reboot && exit'
+alias coldreboot='touch /run/no-kexec-reboot && reboot'
 alias halt='halt && exit'
 alias shutdown='shutdown && exit'
 alias poweroff='poweroff && exit'
--- board/pinetainer/post-build.sh
+++ board/pinetainer/post-build.sh
@@ -5,3 +5,6 @@ rm -f "$1/etc/sudoers.dist" || true
 
 # El inicio del servicio de watchdog es prioritario
 mv "$1/etc/init.d/S15watchdog" "$1/etc/init.d/S00watchdog.sh" || true
+
+# Sustituir llamada a /bin/mount por ejecución de un script
+sed -i '/^mnt::sysinit:/cmnt::sysinit:/etc/init.d/mountfs' "$1/etc/inittab"

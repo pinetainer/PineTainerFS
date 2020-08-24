c744842f8244a85dab7057ef9d457fc0ae135401
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2020-08-24 22:55:12 +0200

    Fix cryptodev compilation, add some final touches to the packages

--- board/pinetainer/post-build.sh
+++ board/pinetainer/post-build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# Tweak network, dnsmasq and udev start order
+# Tweak network, dnsmasq, udev and haveged start order
 mv "${1:?}/etc/init.d/S40network" "${1:?}/etc/init.d/S03network.sh" 2>/dev/null || true
 mv "${1:?}/etc/init.d/S80dnsmasq" "${1:?}/etc/init.d/S10dnsmasq.sh" 2>/dev/null || true
 mv "${1:?}/etc/init.d/S10udev" "${1:?}/etc/init.d/S90udev.sh" 2>/dev/null || true
+mv "${1:?}/etc/init.d/S21haveged" "${1:?}/etc/init.d/S01haveged.sh" 2>/dev/null || true
 
 # Useless Dropbear executables and init scripts
 rm "${1:?}/usr/bin/dropbearkey" 2>/dev/null || true
@@ -22,12 +23,12 @@ rm -rf "${1:?}/usr/html" 2>/dev/null || true
 
 # qemu cruft
 rm -rf "${1:?}/usr/share/qemu/firmware" 2>/dev/null || true
-rm "${1:?}/usr/share/qemu/openbios-*" 2>/dev/null || true
-rm "${1:?}/usr/share/qemu/opensbi-*" 2>/dev/null || true
-rm "${1:?}/usr/share/qemu/efi-*" 2>/dev/null || true
-rm "${1:?}/usr/share/qemu/pxe*" 2>/dev/null || true
-rm "${1:?}/usr/share/qemu/s390*" 2>/dev/null || true
-rm "${1:?}/usr/share/qemu/*.fd" 2>/dev/null || true
+rm "${1:?}"/usr/share/qemu/openbios-* 2>/dev/null || true
+rm "${1:?}"/usr/share/qemu/opensbi-* 2>/dev/null || true
+rm "${1:?}"/usr/share/qemu/efi-* 2>/dev/null || true
+rm "${1:?}"/usr/share/qemu/pxe* 2>/dev/null || true
+rm "${1:?}"/usr/share/qemu/s390* 2>/dev/null || true
+rm "${1:?}"/usr/share/qemu/*.fd 2>/dev/null || true
 
 # iputils cruft
 rm "${1:?}/etc/init.d/ninfod.sh" 2>/dev/null || true
@@ -54,11 +55,20 @@ rmdir "${1:?}/usr/libexec/gnuplot" 2>/dev/null || true
 
 # Configuration files irrelevant to nginx due
 # to its compile options
-rm "${1:?}/etc/nginx/fastcgi*" 2>/dev/null || true
-rm "${1:?}/etc/nginx/scgi*" 2>/dev/null || true
-rm "${1:?}/etc/nginx/uwsgi*" 2>/dev/null || true
-rm "${1:?}/etc/nginx/koi*" 2>/dev/null || true
-rm "${1:?}/etc/nginx/win-*" 2>/dev/null || true
+rm "${1:?}"/etc/nginx/fastcgi* 2>/dev/null || true
+rm "${1:?}"/etc/nginx/scgi* 2>/dev/null || true
+rm "${1:?}"/etc/nginx/uwsgi* 2>/dev/null || true
+rm "${1:?}"/etc/nginx/koi* 2>/dev/null || true
+rm "${1:?}"/etc/nginx/win-* 2>/dev/null || true
 
 # Useless ifupdown script
 rm "${1:?}/etc/network/nfs_check" 2>/dev/null || true
+
+# Useless sudo runtime directory
+rmdir "${1:?}/run/sudo" 2>/dev/null || true
+
+# Useless files in /tmp
+rm -rf "${1:?}"/tmp/* 2>/dev/null || true
+
+# PCI ID list pulled in by mdev that won't be used
+rm "${1:?}/usr/share/pci.ids.gz" 2>/dev/null || true
